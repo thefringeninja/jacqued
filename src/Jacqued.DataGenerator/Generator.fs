3254cb8ec7a63587d6a89b9b409e0eac3e77c0af
João Bragança <495495+thefringeninja@users.noreply.github.com>
2025-03-20 18:34:15 -0700

    add fake data generator to ease testing

---
+++ src/Jacqued.DataGenerator/Generator.fs
@@ -0,0 +1,114 @@
+module Jacqued.Generator
+
+open System
+open Jacqued.Calculate
+
+let private bar = 20m |> Weight |> Bar.Of
+
+let private plates =
+    [ 1.25m; 2.5m; 5m; 5m; 10m; 15m; 20m; 20m ] |> List.map (Weight >> PlatePair)
+
+let private setupGym =
+    seq {
+        "gym",
+        GymSetup
+            { Bar = bar
+              ExercisesDaysPerWeek = ExerciseDaysPerWeek.Four
+              MeasurementSystem = MeasurementSystem.Metric
+              Plates = plates }
+    }
+
+let rec private nextDate date =
+    seq {
+        yield date
+        yield! nextDate (nextExerciseDate ExerciseDaysPerWeek.Four date)
+    }
+let private workout count =
+    let baseWeights =
+        [ (Squats, 150m); (Deadlifts, 120m); (BenchPress, 100m); (OverheadPress, 105m) ]
+        |> List.map (fun (e, w) -> (e, w |> Weight))
+        |> Map.ofList
+
+    let startDate =
+        let today = DateTime.Today
+        let daysToAdd = ((DayOfWeek.Monday |> int) - (today.DayOfWeek |> int) + 7) % 7
+        today.AddDays(daysToAdd |> float)
+
+    let exercises =
+        Exercise.all
+        |> Seq.map (fun e -> e, [ 1..count ] |> List.map (fun _ -> MesocycleId.New()))
+        |> Map.ofSeq
+
+    (([ 0 .. (count - 1) ], Wave.all) ||> Seq.allPairs, Exercise.all)
+    ||> Seq.allPairs
+    |> Seq.zip (nextDate startDate)
+    |> Seq.map (fun (startedAt, ((i, wave), exercise)) ->
+        let mesocycleId = exercises[exercise][i]
+        let streamName = $"mesocycle-{mesocycleId.Value:n}"
+
+        seq {
+            let increase = (if exercise.isLower then 5m else 2.5m) |> Weight
+
+            let oneRepMax = baseWeights[exercise] + (increase * i)
+
+            let trainingMax = oneRepMax * 0.9
+
+            let sets =
+                RepSet.all
+                |> List.allPairs Wave.all
+                |> List.map (fun (wave, set) ->
+                    let weight, reps, _ = Calculate.set wave set bar plates trainingMax
+
+                    ((wave, set), (weight, reps)))
+
+                |> Map.ofList
+
+            if wave = Wave.One then
+                yield
+                    streamName,
+                    MesocycleStarted
+                        { MeasurementSystem = MeasurementSystem.Metric
+                          MesocycleId = mesocycleId
+                          OneRepMax = oneRepMax
+                          TrainingOneRepMax = trainingMax
+                          StartedAt = startedAt
+                          WorkoutPlan = { Exercise = exercise; Sets = sets } }
+
+            yield!
+                RepSet.all
+                |> Seq.map (fun repSet ->
+                    let weight, reps = sets[(wave, repSet)]
+
+                    streamName,
+                    RepSetCompleted
+                        { Exercise = exercise
+                          MesocycleId = mesocycleId
+                          RepSet = repSet
+                          Weight = weight
+                          Wave = wave
+                          Reps = reps
+                          CompletedAt = startedAt })
+
+            yield
+                streamName,
+                WaveCompleted
+                    { MesocycleId = mesocycleId
+                      Exercise = exercise
+                      CompletedAt = startedAt
+                      Wave = wave }
+
+            if wave = Wave.Four then
+                yield
+                    streamName,
+                    MesocycleCompleted
+                        { Exercise = exercise
+                          MesocycleId = mesocycleId
+                          SuggestedOneRepMax = baseWeights[exercise] + (increase * (i + 1)) }
+        })
+    |> Seq.concat
+
+let generate count =
+    seq {
+        yield! setupGym
+        yield! workout count
+    }
---
+++ src/Jacqued.DataGenerator/Program.fs
@@ -0,0 +1,54 @@
+﻿open System
+open System.IO
+open FSharp.Control
+open FSharp.SystemCommandLine
+open Jacqued
+open Jacqued.EventStorage
+open Microsoft.Data.Sqlite
+open SqlStreamStore
+
+let handler (count, output:FileInfo) =
+    let cs = SqliteConnectionStringBuilder()
+    cs.DataSource <- output.FullName
+
+    let settings = SqliteStreamStoreSettings(cs.ToString())
+    settings.GetUtcNow <- (fun () -> DateTime.UtcNow)
+    let streamStore = new SqliteStreamStore(settings)
+    streamStore.CreateSchemaIfNotExists()
+
+    let events = Generator.generate count
+
+    let append d =
+        let streamName = d |> fst
+        let e = [ d |> snd ]
+        appendToStream streamStore streamName -2 e |> ignore
+
+    events |> Seq.iter append
+
+    // streamStore.ReadAllForwards()
+    // |> TaskSeq.iterAsync (fun message ->
+    //     task {
+    //         let! data = message.GetJsonData()
+    //         printfn $"""{{ "streamName": "{message.StreamId}", "type": "{message.Type}", "data": {data} }}"""
+    //     })
+    // |> ignore
+    0
+
+[<EntryPoint>]
+let main argv =
+    rootCommand argv {
+        inputs (
+            Input.Option<int>(
+                aliases = [ "--count" ],
+                description = "The number of mesocycles to complete for each exercise",
+                defaultValue = 1
+            ),
+            Input.Option<FileInfo>(
+                aliases = [ "--output" ],
+                description = "The path to the output database",
+                defaultValue = FileInfo("jacqued.db")
+            )
+        )
+
+        setHandler handler
+    }
